6ea713843fa6295cc19e2ae6207b19bf71505d71
井古田光晴 <ikota.mitsuharu@adways.net>
2015-01-14 22:18:03 +0900

    add practice

---
+++ Practice/SideEffect.scala
@@ -0,0 +1,15 @@
+object SideEffect {
+  var total = 0
+
+  def main(args: Array[String]) {
+    var add = (x:Int) => {
+      total += x
+      total
+    }
+
+    printf("total=%s\n", add(1))
+    printf("total=%s\n", add(1))
+    printf("total=%s\n", add(1))
+  }
+  // main(Array())
+}
