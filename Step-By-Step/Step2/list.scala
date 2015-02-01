bedeae3865b88424d84b9aafe3810e8c2210691e
ikotan <ikotan.69@gmail.com>
2015-02-01 14:11:57 +0000

    add step2

---
+++ Step-By-Step/Step2/list.scala
@@ -0,0 +1,13 @@
+val oneTwo = List(1, 2)
+val threeFour = List(3, 4)
+val oneTwoThreeFour = oneTwo ::: threeFour
+println(oneTwo + " and " + threeFour + " were not mutated.")
+println("Thus, " + oneTwoThreeFour + " is a new list.")
+
+val twoThree = List(2, 3)
+val oneTwoThree = 1 :: twoThree
+println(oneTwoThree)
+
+val oneTwoThree2 = 1 :: 2 :: 3 :: Nil
+println(oneTwoThree2)
+
